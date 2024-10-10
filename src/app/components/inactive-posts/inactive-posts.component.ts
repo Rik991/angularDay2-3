eab3e9add70bf1c6e2cbb250e0364bfa1f5c35c7
Riccardo Santilli <riccardosantilli@live.it>
2024-10-10 16:44:52 +0200

    second commit

--- src/app/components/inactive-posts/inactive-posts.component.ts
+++ src/app/components/inactive-posts/inactive-posts.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Post } from '../../interfaces/post';
 
 @Component({
   selector: 'app-inactive-posts',
   templateUrl: './inactive-posts.component.html',
-  styleUrl: './inactive-posts.component.scss'
+  styleUrl: './inactive-posts.component.scss',
 })
 export class InactivePostsComponent {
+  posts: Post[] = [];
+  indexRandom: number = 0;
+  arrayOther: Post[] = [];
 
+  ngOnInit() {
+    //metodo lanciato al caricamento del componente (per usare ts)
+    fetch('db.json')
+      .then((res) => res.json())
+      .then((dati) => {
+        this.posts = dati.posts;
+        console.log(this.posts);
+        this.indexRandom = Math.floor(Math.random() * 29);
+        for (let i = 4; i < this.posts.length; i++) {
+          this.arrayOther.push(this.posts[i]);
+        }
+      });
+  }
 }
--- src/app/components/post-detail/post-detail.component.ts
+++ src/app/components/post-detail/post-detail.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Post } from '../../interfaces/post';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-post-detail',
   templateUrl: './post-detail.component.html',
-  styleUrl: './post-detail.component.scss'
+  styleUrl: './post-detail.component.scss',
 })
 export class PostDetailComponent {
+  constructor(private route: ActivatedRoute) {}
+  post!: Post;
+  posts: Post[] = [];
 
+  ngOnInit() {
+    //metodo lanciato al caricamento del componente (per usare ts)
+    fetch('db.json')
+      .then((res) => res.json())
+      .then((dati) => {
+        this.posts = dati.posts;
+
+        //recupero i dettagli della rotta
+        this.route.params.subscribe((params: any) => {
+          const singlePost = this.posts.find((p) => p.id == params.id);
+          if (singlePost) {
+            this.post = singlePost;
+          }
+        });
+      });
+  }
 }
